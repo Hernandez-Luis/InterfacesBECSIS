ed3701d9750bd38ddf89b8014f03ab14a40a666e
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2024-04-07 22:14:06 -0600

    modificacion de footer y nav

---
+++ javaScript.js/funcionamiento.js
@@ -0,0 +1,9 @@
+// Función para mostrar la ventana modal
+function mostrarModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+// Función para cerrar la ventana modal
+function cerrarModal() {
+    document.getElementById('modal').style.display = 'none';
+}
